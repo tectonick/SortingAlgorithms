d581110f9572e77085ae2c599999b609d43fa453
Nick <korn9509@gmail.com>
2019-08-07 00:25:16 +0300

    Init test

--- Sorting Test/MainForm.h
+++ Sorting Test/MainForm.h
@@ -48,11 +48,17 @@ namespace SortingTest {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MainForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(851, 339);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
